a732c83bd24ecca86ac36dbd813abe211285923e
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2023-06-04 11:45:47 +0600

    side nav updated

--- qtec/settings.py
+++ qtec/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 from django.conf import settings
 from django.conf.urls.static import static
 BASE_DIR = Path(__file__).resolve().parent.parent
